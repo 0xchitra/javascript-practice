d305e76bc531de5d5a861ccd749df509914d6ef4
Chitra <withchitra@gmail.com>
2026-07-21 15:18:52 +0530

    conditional beginner question solve

--- Conditional Statements/solved - conditions.js
+++ Conditional Statements/solved - conditions.js
@@ -1,9 +1,94 @@
 //Q1
-let num = -6;
+let num = 6;
 if (num > 0) {
   console.log("positive");
-} else if ((num = 0)) {
+} else if (num === 0) {
   console.log("zero");
 } else {
   console.log("negative");
+}
+
+//Q2
+let num1 = 0;
+if (num1 > 0) {
+  console.log("positive");
+} else if (num1 === 0) {
+  console.log("zero");
+} else {
+  console.log("negative");
+}
+
+//Q3
+let num2 = -6;
+if (num2 > 0) {
+  console.log("positive");
+} else if (num2 === 0) {
+  console.log("zero");
+} else {
+  console.log("negative");
+}
+
+//Q4
+let num4 = 78;
+if (num4 % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+//Q5
+let age = 17;
+if (age >= 18) {
+  console.log("eligible to vote");
+} else {
+  console.log("not eligible to vote");
+}
+
+//Q6
+let agE = 17;
+if (agE >= 18) {
+  console.log("eligible for driving license");
+} else {
+  console.log("not eligible for driving license");
+}
+
+//7
+let numA = 78;
+let numB = 79;
+if (numA > numB) {
+  console.log(`${numA} is greater`);
+} else {
+  console.log(`${numB} is greater`);
+}
+
+//Q8
+let numC = 93;
+if (numA > numB) {
+  console.log(`${numA} is greater`);
+} else if (numB > numC) {
+  console.log(`${numB} is greater`);
+} else {
+  console.log(`${numC} is greater`);
+}
+
+//Q9
+let year = 2024;
+if (year % 4 === 0) {
+  console.log("leap year");
+} else {
+  console.log("not leap year");
+}
+
+//Q10
+let char = "i";
+if (
+  char === "a" ||
+  char === "e" ||
+  char === "i" ||
+  char === "o" ||
+  char === "u"
+) {
+  console.log("the character is a vewel");
+} else {
+  console.log("the character is not a vowel");
 }
