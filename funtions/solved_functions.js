2c58b509a5881b67482aeba55c9340009736fcdb
Chitra <withchitra@gmail.com>
2026-07-28 11:32:47 +0530

    solve beginner functions question

---
+++ funtions/solved_functions.js
@@ -0,0 +1,52 @@
+//1
+
+//2
+function greet() {
+  console.log("hello, world!");
+}
+
+//3
+greet();
+greet();
+greet();
+
+//4
+function myName() {
+  console.log("chitra");
+}
+
+//5
+function greets(v1) {
+  console.log(`hello! ${v1}`);
+}
+greets("chitra");
+
+//6
+function add(v1, v2) {
+  console.log(v1 + v2);
+}
+add(3, 5);
+
+//7
+function substracts(v1, v2) {
+  console.log(v1 - v2);
+}
+substracts(5, 2);
+
+//8
+function multiple(v1, v2) {
+  console.log(v1 * v2);
+}
+multiple(3, 3);
+
+//9
+function divide(v1, v2) {
+  console.log(v1 / v2);
+}
+divide(8, 2);
+
+//10
+function square(v1) {
+  console.log(v1 * v1);
+}
+square(3);
