013e26589c2979ad62316392ae798a69abd273ef
Chitra <withchitra@gmail.com>
2026-07-30 00:31:08 +0530

    basic methods strings questions solve

--- strings/solved_strings.js
+++ strings/solved_strings.js
@@ -47,3 +47,67 @@ for (let i = str.length - 1; i >= 0; i--) {
   reverse += str[i];
 }
 console.log(reverse);
+
+//11
+let countVowel = 0;
+for (let i = 0; i <= str.length - 1; i++) {
+  if (
+    str[i] === "a" ||
+    str[i] === "e" ||
+    str[i] === "i" ||
+    str[i] === "o" ||
+    str[i] === "u"
+  ) {
+    countVowel++;
+  }
+}
+console.log(countVowel);
+
+//12
+let countCons = 0;
+for (let i = 0; i <= str.length - 1; i++) {
+  if (
+    str[i] !== "a" ||
+    str[i] !== "e" ||
+    str[i] !== "i" ||
+    str[i] !== "o" ||
+    str[i] !== "u"
+  ) {
+    countCons++;
+  }
+}
+console.log(countCons);
+
+//13
+if (str.includes("world")) {
+  console.log(true);
+}
+
+//14
+console.log(str.indexOf("w"));
+
+//15
+console.log(str.lastIndexOf("o"));
+
+//16
+let js = "i love java";
+let jsMod = js.replace("java", "javascript");
+console.log(jsMod);
+
+//17
+let poem = "hello cat, if you are a cat, you should not berk like dog";
+let poemMod = poem.replaceAll("cat", "dog");
+console.log(poemMod);
+
+//18
+console.log(poem.slice(0, 5));
+console.log(poem.substring(0, 5));
+
+//19
+// slice(); //negative value indices to count backward
+// substring(); //negative value indices as 0
+// substr(); //avoid in modern js
+
+//20
+let space = " chitra  ";
+console.log(space.trim());
