7160f6c8d4fad209996f05b34390aa37da7f3dfa
Chitra <withchitra@gmail.com>
2026-08-06 12:33:00 +0530

    solve 5 questions of object

--- objects/solved_objects.js
+++ objects/solved_objects.js
@@ -1,2 +1,29 @@
+//1
 let person = { name: "Chitra", age: 20, city: "Nagaon" };
+console.log(person);
+
+//2
+const car = {
+  brand: "Honda",
+  model: "City",
+  year: 2024,
+};
+console.log(car.brand);
+console.log(car.model);
+console.log(car.year);
+
+console.log(car["brand"]);
+console.log(car["model"]);
+console.log(car["year"]);
+
+//3
+person.age = 21;
+console.log(person);
+
+//4
+person.email = "test@hello.com";
+console.log(person);
+
+//5
+delete person.city;
 console.log(person);
