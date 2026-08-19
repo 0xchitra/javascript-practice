570c7199c5ce0096bba4abd8c9f84a045e3405d0
Chitra <withchitra@gmail.com>
2026-08-19 22:11:21 +0530

    solve 50 questions of DOM

--- dom/style_manipulation.js
+++ dom/style_manipulation.js
@@ -18,10 +18,36 @@ title.style.borderRadius = "8px";
 //44
 let btn = document.querySelector("#Btn");
 btn.addEventListener("click", () => {
-  btn.style.backgroundColor = "#00e9d6";
-  btn.style.width = "180px"
-  btn.style.height = "120px"
+  btn.style.backgroundColor = "rgb(93, 195, 142)";
+  btn.style.width = "180px";
+  btn.style.height = "120px";
+  btn.style.transition = "all .5s";
 });
 
 //45 element.style.color = ""
 // This removes the inline color style from the element.
+
+//46
+// .style = Directly modifies inline styles
+// .classList = Adds/removes CSS classes
+
+//47
+// classList represent the collection of css classes applied to an element
+
+//48
+// classList.add() = element.classList.add("active");
+// result = <div class="active"></div>
+
+//classList.remove() = removes a class
+// classList.toggle() = adds the class if it doesn't exist and removes it if it does.
+// classList.contains() = Checks whether an element has a particular class.
+// classList.replace() = replace a class, element.classList.replace("old", "new");
+
+//49
+title.classList.add("head");
+let head = document.querySelector(".head");
+head.style.color = "blue";
+
+//50
+head.classList.remove("head");
+
