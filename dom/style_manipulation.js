5343b2c7d9f7332765cff80c1261767c4e5fdbc1
Chitra <withchitra@gmail.com>
2026-08-19 20:18:14 +0530

    45 questions solve of DOM

--- dom/style_manipulation.js
+++ dom/style_manipulation.js
@@ -1,1 +1,27 @@
 //41
+let title = document.querySelector("h1");
+title.style.color = "red";
+
+//42
+title.style.backgroundColor = "pink";
+title.style.fontSize = "45px";
+title.style.width = "200px";
+title.style.height = "240px";
+title.style.border = "12px solid orange";
+title.style.borderRadius = "8px";
+
+//43
+// This is necessary because background-color would otherwise be interpreted by JavaScript as background - color
+// CSS uses kebab-case(background-color)
+// JS uses camelCase(backgroundColor)
+
+//44
+let btn = document.querySelector("#Btn");
+btn.addEventListener("click", () => {
+  btn.style.backgroundColor = "#00e9d6";
+  btn.style.width = "180px"
+  btn.style.height = "120px"
+});
+
+//45 element.style.color = ""
+// This removes the inline color style from the element.
