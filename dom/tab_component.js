92575c451c2fbf537bce4a2721220effc21f4028
Chitra <withchitra@gmail.com>
2026-08-21 14:30:01 +0530

    solve Q59 tab component

--- dom/tab_component.js
+++ dom/tab_component.js
@@ -1,1 +1,21 @@
-//
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", function () {
+    tabs.forEach((tab) => {
+      tab.classList.remove("active");
+    });
+
+    tab.classList.add("active");
+
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    
+    const tabId = tab.dataset.tab;
+    const content = document.querySelector(`#${tabId}`);
+
+    content.classList.add("active");
+  });
+});
