bfa582247f137b92e524fad8797e7432f9a84a14
Chitra <withchitra@gmail.com>
2026-08-21 13:44:48 +0530

    solve Q56 character counter

--- dom/character_counter.js
+++ dom/character_counter.js
@@ -1,1 +1,7 @@
-//
+let textArea = document.querySelector("#message");
+let counter = document.querySelector("#counts");
+
+textArea.addEventListener("input", function () {
+  let chars = textArea.value.length;
+  counter.textContent = `Total Character: ${chars}`
+});
