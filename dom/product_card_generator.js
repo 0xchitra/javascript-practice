9947eaf55cbb6558757d9903e2be7d38917af694
Chitra <withchitra@gmail.com>
2026-08-21 14:06:51 +0530

    solve Q58 product card generator

--- dom/product_card_generator.js
+++ dom/product_card_generator.js
@@ -1,5 +1,22 @@
 const products = [
   { name: "Laptop", price: 50000 },
   { name: "Mouse", price: 1000 },
-  { name: "Keyboard", price: 2000 }
+  { name: "Keyboard", price: 2000 },
 ];
+
+const productContainer = document.querySelector("#products");
+
+products.forEach((product) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const name = document.createElement("h2");
+  name.textContent = product.name;
+
+  const price = document.createElement("p");
+  price.textContent = `₹${product.price}`;
+
+  card.append(name, price);
+
+  productContainer.appendChild(card);
+});
